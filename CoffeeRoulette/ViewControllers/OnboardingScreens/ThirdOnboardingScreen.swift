21faafefccab9223a79471400a7e83667dfd8c30
WillChew <williamchew95@gmail.com>
2018-08-08 13:12:46 -0400

    added onboarding screens

--- CoffeeRoulette/ViewControllers/OnboardingScreens/ThirdOnboardingScreen.swift
+++ CoffeeRoulette/ViewControllers/OnboardingScreens/ThirdOnboardingScreen.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreLocation
+import ChameleonFramework
+
 
 class ThirdOnboardingScreen: UIViewController, CLLocationManagerDelegate {
 
@@ -16,10 +18,17 @@ class ThirdOnboardingScreen: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .brown
+        
+        self.view.backgroundColor = UIColor(gradientStyle: UIGradientStyle.topToBottom, withFrame: self.view.frame, andColors: [UIColor.black, UIColor(red:0.3, green:0.3, blue:0.3, alpha:1.0)])
+        
         getStartedButton.backgroundColor = UIColor(red:0.75, green:0.63, blue:0.45, alpha:1.0)
         getStartedButton.setTitleColor(UIColor(red:0.27, green:0.22, blue:0.14, alpha:1.0), for: .normal)
-        getStartedButton.layer.cornerRadius = getStartedButton.frame.height / 2
+        
+            getStartedButton.layer.borderWidth = 2.5
+        
+        getStartedButton.layer.borderColor = UIColor(red:0.15, green:0.15, blue:0.15, alpha:1.0).cgColor
+        
+        getStartedButton.layer.cornerRadius = getStartedButton.frame.height / 4
         
          locationManager = CLLocationManager()
             locationManager.requestWhenInUseAuthorization()
